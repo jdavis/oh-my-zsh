ded9ed9d864ce1e4b9cbee52ea42581e35c3f21b
Josh Davis <joshuadavis42@gmail.com>
2012-11-19 06:31:36 -0600

    Added a sweet crypt/decrypt feature.

---
+++ custom/crypt.zsh
@@ -0,0 +1,25 @@
+function load-crypt() {
+    SECRET=~/.oh-my-zsh/custom/secret.gpg
+    TEMPPREFIX='crypt'
+    TEMPFILE=`mktemp -q /tmp/${TEMPPREFIX}.XXXXXXXXX`
+
+    if [ $? -ne 0 ]; then
+       echo "$0: Can't create temp file, exiting..."
+       return 1
+    fi
+
+    if [ -z "$CRYPT_CIPHER" ]; then
+        echo "To select a cipher, set \$CRYPT_CIPHER"
+        return 1
+    fi
+    
+    
+    hash gpg 2> /dev/null || {
+        echo "Oh dear, I require gpg to work. Consider installing it."
+        return 1
+    }
+
+    gpg --no-mdc-warning --cipher-algo ${CRYPT_CIPHER} -d ${SECRET} >> ${TEMPFILE}
+    source ${TEMPFILE}
+    rm -f ${TEMPFILE}
+}
