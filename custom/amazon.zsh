b391596d9d0568c617b322047a0abf9111e3c808
Josh Davis <josh@joshldavis.com>
2013-06-13 16:52:08 -0700

    No allow_other on SSHFS

--- custom/amazon.zsh
+++ custom/amazon.zsh
@@ -1,3 +1,3 @@
 alias rhel-start='VBoxManage startvm "RHEL5 64-bit desktop" --type headless'
-alias rhel-fs='mkdir -p ~/Programming/RHEL/workspace 2>1 /dev/null; sshfs -o allow_other -o idmap=user $USER@$USER.desktop:/workplace/$USER ~/Programming/RHEL/workspace'
+alias rhel-fs='mkdir -p ~/Programming/RHEL/workspace 2>1 /dev/null; sshfs -o idmap=user $USER@$USER.desktop:/workplace/$USER ~/Programming/RHEL/workspace'
 alias rhel-ssh='ssh $USER@$USER.desktop'
