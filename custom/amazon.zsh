a87150988b06dad5690cb566003044e901ad4151
Josh Davis <josh@joshldavis.com>
2013-06-24 12:44:27 -0700

    Fix tmux ssh issues

--- custom/amazon.zsh
+++ custom/amazon.zsh
@@ -1,3 +1,3 @@
 alias rhel-start='VBoxManage startvm "RHEL5 64-bit desktop" --type headless'
 alias rhel-fs='mkdir -p ~/Programming/RHEL/workspace >> /dev/null 2>&1; sshfs -o idmap=user -o nonempty $USER@$USER.desktop:/workplace/$USER ~/Programming/RHEL/workspace'
-alias rhel-ssh='ssh $USER@$USER.desktop'
+alias rhel-ssh='TERM=xterm ssh $USER@$USER.desktop'
