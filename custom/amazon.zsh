2aac9ab8c4a8d14e21e7e36f3fdd963b9c06fa3e
Josh Davis <josh@joshldavis.com>
2013-06-24 12:53:57 -0700

    Remove amazon.zsh

--- custom/amazon.zsh
+++
@@ -1,3 +0,0 @@
-alias rhel-start='VBoxManage startvm "RHEL5 64-bit desktop" --type headless'
-alias rhel-fs='mkdir -p ~/Programming/RHEL/workspace >> /dev/null 2>&1; sshfs -o idmap=user -o nonempty $USER@$USER.desktop:/workplace/$USER ~/Programming/RHEL/workspace'
-alias rhel-ssh='TERM=xterm ssh $USER@$USER.desktop'
