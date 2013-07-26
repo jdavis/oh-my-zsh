29306dd05952a4d160ba6597630a57e05fc1778b
Josh Davis <josh@joshldavis.com>
2013-07-26 08:39:49 -0700

    Fix up z.zsh paths

--- custom/z.zsh
+++ custom/z.zsh
@@ -1,12 +1,12 @@
 # .z is taken by my submodule
 # Change it to ~/.zdata
-if [[ ! -f "~/.zdata" ]] ; then
+export _Z_DATA=$HOME/.zdata
+
+if [[ ! -f $_Z_DATA ]] ; then
     touch ~/.zdata
 fi
 
-_Z_DATA='~/.zdata'
-
 # Load z.sh if it exists
-if [[ -d "~/.z" && -f "~/.z/z.sh" ]] ; then
+if [[ -d "$HOME/.z" && -f "$HOME/.z/z.sh" ]] ; then
     . ~/.z/z.sh
 fi
