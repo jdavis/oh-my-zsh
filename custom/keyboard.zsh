cee81ae82d0c1b421185df893b6c2e5671294d1b
Josh Davis <josh@joshldavis.com>
2013-08-13 23:16:45 -0500

    Add check for OS X

--- custom/keyboard.zsh
+++ custom/keyboard.zsh
@@ -1,7 +1,13 @@
+# Don't do xmodmap on OS X
+if [[ $CURRENT_OS == 'OS X' ]]; then
+    exit
+fi
+
 # Check to make sure xmodmap exists
 hash xmodmap 2> /dev/null || {
     exit
 }
+
 
 # Load Vim xmodmap
 if [ -s ~/.xmodmap-vim ]; then
