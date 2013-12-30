ca8558ac62acb8dde6f825d8a19ff09058f0c820
Josh Davis <josh@joshldavis.com>
2013-12-30 11:47:40 -0500

    Add CentOS plugin

---
+++ plugins/centos/centos.plugin.zsh
@@ -0,0 +1,24 @@
+# Helper Function for chkconfig
+
+startup() {
+    if [ $# -lt 2 ]; then
+        echo "Usage: startup {on|off} <service>"
+        return 1
+    fi
+
+
+    case "$1" in
+        on)
+            sudo chkconfig --add $2
+            sudo chkconfig $2 on
+            ;;
+        off)
+            sudo chkconfig $2 off
+            ;;
+        *)
+            echo "Usage: startup {on|off} <service>"
+            return 1
+            ;;
+    esac
+}
+
