16a763f6a908b76785468d70a9a1eaf3aa26cc64
Josh Davis <josh@joshldavis.com>
2014-01-09 22:33:03 -0600

    Keep finding tabs...

--- custom/paths.zsh
+++ custom/paths.zsh
@@ -13,24 +13,24 @@ export PATH=$PATH:~/bin
 if [[ $CURRENT_OS == 'OS X' ]]; then
     # Prefix /usr/local/bin for brew
     export PATH=/usr/local/bin:$PATH
-	# MIT-Scheme
-	export MITSCHEME_LIBRARY_PATH=/usr/local/lib/mit-scheme-c/
-	# Nmap
-	export PATH=$PATH:/Users/Davis/.nmap
-	#  Django
-	export PATH=$PATH:/Library/Python/2.5/site-packages/django/bin
-	#  MacPorts
-	export PATH=$PATH:/opt/local/bin
-	#  Add the bin/Mac directory
-	export PATH=$PATH:~/bin/Mac
+    # MIT-Scheme
+    export MITSCHEME_LIBRARY_PATH=/usr/local/lib/mit-scheme-c/
+    # Nmap
+    export PATH=$PATH:/Users/Davis/.nmap
+    #  Django
+    export PATH=$PATH:/Library/Python/2.5/site-packages/django/bin
+    #  MacPorts
+    export PATH=$PATH:/opt/local/bin
+    #  Add the bin/Mac directory
+    export PATH=$PATH:~/bin/Mac
     # Add all npm packages
     export PATH=$PATH:/usr/local/share/npm/bin
 
     # Add Java jars (like checkstyle, etc)
     export PATH=$PATH:~/jars
 elif [[ $CURRENT_OS == 'Linux' ]]; then
-	#  Add the bin/Arch directory
-	export PATH=$PATH:~/bin/Arch
+    #  Add the bin/Arch directory
+    export PATH=$PATH:~/bin/Arch
     export PATH=$PATH:~/bin/appengine
     export PATH=$PATH:~/.cabal/bin
     export PATH=$PATH:~/bin/
